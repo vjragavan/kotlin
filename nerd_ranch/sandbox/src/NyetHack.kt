cf0336d6024775b8795099b61f9975c6de203b0e
vjragavan <vijay@onepeloton.com>
2018-07-23 20:57:35 -0400

    added Ranges

--- nerd_ranch/sandbox/src/NyetHack.kt
+++ nerd_ranch/sandbox/src/NyetHack.kt
@@ -5,26 +5,17 @@ fun main(args: Array<String>) {
     var isImmortal = false
     var isAuraVisible = healthPoints > 50 && isBlessed || isImmortal
 
-    var healthStatus = if (healthPoints == 100)
-        "is in Good Health"
-    else if (healthPoints >= 90)
-        "has few scratches"
-    else if (healthPoints >= 75) {
-        if (isBlessed)
-            "has minor injuries, but will heal quickly"
-        else
-            "has minor injuries"
-    }
-    else if (healthPoints >= 15)
-        "is badly wounded"
-    else
-        "is in awful state"
+    var healthStatus = if (healthPoints == 100) "is in Good Health"
+    else if (healthPoints in 90..99) "has few scratches"
+    else if (healthPoints in 75..89)
+        if (isBlessed) "has minor injuries, but will heal quickly"
+        else "has minor injuries"
+    else if (healthPoints in 15..74) "is badly wounded"
+    else "is in awful state"
 
-    //player state
     println("$name $healthStatus")
 
-    if (isAuraVisible)
-        println("Green Aura")
-    else
-        println("No Aura")
+    var auraStatus = if (isAuraVisible) "Green Aura" else "No Aura"
+
+    println("$name $auraStatus")
 }
