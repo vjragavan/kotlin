4fd931a9d3aef281c73180f284094891ac1b7dcd
vjragavan <vijay@onepeloton.com>
2018-07-23 21:04:30 -0400

    added Conditional Expression using When

--- nerd_ranch/sandbox/src/NyetHack.kt
+++ nerd_ranch/sandbox/src/NyetHack.kt
@@ -5,13 +5,16 @@ fun main(args: Array<String>) {
     var isImmortal = false
     var isAuraVisible = healthPoints > 50 && isBlessed || isImmortal
 
-    var healthStatus = if (healthPoints == 100) "is in Good Health"
-    else if (healthPoints in 90..99) "has few scratches"
-    else if (healthPoints in 75..89)
-        if (isBlessed) "has minor injuries, but will heal quickly"
-        else "has minor injuries"
-    else if (healthPoints in 15..74) "is badly wounded"
-    else "is in awful state"
+    var healthStatus = when (healthPoints) {
+        100 -> "is in Good Health"
+        in 90..99 -> "has few scratches"
+        in 75..89 -> {
+            if (isBlessed) "has minor injuries, but will heal quickly"
+            else "has minor injuries"
+        }
+        in 15..74 -> "is badly wounded"
+        else -> "is in awful state"
+    }
 
     println("$name $healthStatus")
 
