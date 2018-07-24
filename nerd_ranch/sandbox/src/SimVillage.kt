8400823801fbb6486f8bff9349dcac4da532cf4b
vjragavan <vijay@onepeloton.com>
2018-07-24 09:35:38 -0400

    it

--- nerd_ranch/sandbox/src/SimVillage.kt
+++ nerd_ranch/sandbox/src/SimVillage.kt
@@ -1,7 +1,9 @@
 fun main(Args: Array<String>) {
-    val greetingFunction: (String) -> String = { name ->
+    //when there is only one argument in the anonymous function
+    //you can reference it as 'it' immaterial of the type
+    val greetingFunction: (String) -> String = {
         val currentYear = 2018
-        "welcome to SimVillage, Mayor ${name}, Copyright ${currentYear}"
+        "welcome to SimVillage, Mayor ${it}, Copyright ${currentYear}"
     }
 
     println(greetingFunction("Vijay"))
