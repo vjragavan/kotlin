34228868b62c66267c1b76e377aa1b2433b5879a
vjragavan <vijay@onepeloton.com>
2018-07-24 09:25:12 -0400

    anonymous function

--- nerd_ranch/sandbox/src/NyetHack.kt
+++ nerd_ranch/sandbox/src/NyetHack.kt
@@ -1,23 +1,43 @@
 fun main(args: Array<String>) {
     val name = "Vijay"
     var healthPoints = 89
-    var isBlessed = true
+    var isBlessed = false
     var isImmortal = false
     var isAuraVisible = healthPoints > 50 && isBlessed || isImmortal
 
+    
     var healthStatus = formatHealthStatus(healthPoints, isBlessed)
     println("$name $healthStatus")
-    printPlayerStatus(isAuraVisible, name)
-    castFireBall(3)
+    printPlayerStatus(isAuraVisible = isAuraVisible, name = name)
+
+    //calling a function with a return type
+    var inebiratedLevel = castFireBall(numOfBalls = 53)
+
+    //conditional expression using when
+    var inebiratedStatus = when (inebiratedLevel) {
+        in 1..10 -> "tipsy"
+        in 11..20 -> "sloshed"
+        in 21..30 -> "soused"
+        in 31..40 -> "stewed"
+        in 41..50 -> "..t0aSt3d"
+        else -> ""
+    }
+    println("Inebirated status is $inebiratedStatus")
 }
 
-private fun castFireBall(numOfBalls: Int = 2) =
+private fun castFireBall(numOfBalls: Int = 2): Int {
     println("A glass of fireball springs into existence (x$numOfBalls)")
+    if (numOfBalls > 50)
+        return 50
+    return numOfBalls
+}
 
 private fun printPlayerStatus(isAuraVisible: Boolean, name: String) {
-    var auraStatus = if (isAuraVisible) "Green Aura" else "No Aura"
-    println("$name $auraStatus")
+    println("$name ${auraColor(isAuraVisible)}")
 }
+
+private fun auraColor(isAuraVisible:Boolean) =
+        if (isAuraVisible) "Green Aura" else "No Aura"
 
 private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) =
    when (healthPoints) {
---
+++ nerd_ranch/sandbox/src/SimVillage.kt
@@ -0,0 +1,6 @@
+fun main(Args: Array<String>) {
+    println({
+        val currentYear = 2018
+        "Welcome to SimVillage, Mayor! Copyright ${currentYear}"
+    }())
+}
