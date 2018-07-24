980de38b5fc5fea31c61f26b74056a7df38c57ef
vjragavan <vijay@onepeloton.com>
2018-07-24 12:22:55 -0400

    shorthand syntax for functions being passed as parameters to functions

--- nerd_ranch/sandbox/src/SimVillage.kt
+++ nerd_ranch/sandbox/src/SimVillage.kt
@@ -1,13 +1,12 @@
 fun main(Args: Array<String>) {
 
-    val greetingFunction = { name: String, numBuilding: Int->
+    //passing the function as a parameter
+    //don't have to specify the type of the arguments
+    runSimulation("Vijay", { name, numBuilding->
         val currentYear = 2018
         println("Adding $numBuilding buildings")
         "Welcome to SimVillage, Mayor $name, Copyright $currentYear"
-    }
-
-    runSimulation("Vijay", greetingFunction)
-
+    })
 }
 
 fun runSimulation(name: String, func: (String, Int) -> String) {
