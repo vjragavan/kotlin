f1d63d1b8df59398ff394187b6fb3778b26f0547
vjragavan <vijay@onepeloton.com>
2018-07-23 22:19:10 -0400

    intro to functions

--- nerd_ranch/sandbox/src/NyetHack.kt
+++ nerd_ranch/sandbox/src/NyetHack.kt
@@ -5,6 +5,22 @@ fun main(args: Array<String>) {
     var isImmortal = false
     var isAuraVisible = healthPoints > 50 && isBlessed || isImmortal
 
+    var healthStatus = formatHealthStatus(healthPoints, isBlessed)
+    println("$name $healthStatus")
+    printPlayerStatus(isAuraVisible, name)
+    castFireBall()
+}
+
+private fun castFireBall() {
+    println("A glass of fireball springs into existence")
+}
+
+private fun printPlayerStatus(isAuraVisible: Boolean, name: String) {
+    var auraStatus = if (isAuraVisible) "Green Aura" else "No Aura"
+    println("$name $auraStatus")
+}
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
     var healthStatus = when (healthPoints) {
         100 -> "is in Good Health"
         in 90..99 -> "has few scratches"
@@ -15,10 +31,5 @@ fun main(args: Array<String>) {
         in 15..74 -> "is badly wounded"
         else -> "is in awful state"
     }
-
-    println("$name $healthStatus")
-
-    var auraStatus = if (isAuraVisible) "Green Aura" else "No Aura"
-
-    println("$name $auraStatus")
+    return healthStatus
 }
