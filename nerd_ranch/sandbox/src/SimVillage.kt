fad68b75ba3a5a26e0b41318913697bd1db2b2fb
vjragavan <vijay@onepeloton.com>
2018-07-24 09:33:26 -0400

    arguments in anonymous functions

--- nerd_ranch/sandbox/src/SimVillage.kt
+++ nerd_ranch/sandbox/src/SimVillage.kt
@@ -1,8 +1,8 @@
 fun main(Args: Array<String>) {
-    val greetingFunction: () -> String = {
+    val greetingFunction: (String) -> String = { name ->
         val currentYear = 2018
-        "welcome to SimVillage, Mayor! Copyright ${currentYear}"
+        "welcome to SimVillage, Mayor ${name}, Copyright ${currentYear}"
     }
 
-    println(greetingFunction())
+    println(greetingFunction("Vijay"))
 }
